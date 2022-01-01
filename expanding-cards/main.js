c21a9cbee2d6a6c1e01ae338c10cbdc1c110122c
Rodrigo Luiz <dolbrebr@gmail.com>
2021-12-31 21:46:06 -0300

    primeiro projeto dia 01

---
+++ expanding-cards/main.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActivesClasses()
+    panel.classList.toggle('active')
+  })
+})
+
+function removeActivesClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active')
+  })
+}
